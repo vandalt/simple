2585cda83da33e506f244cd0e55b0a07ca9f716c
Thomas Vandal <thomas.vandal@umontreal.ca>
2025-06-24 19:35:31 -0400

    Document chainplot

--- src/simple/plot.py
+++ src/simple/plot.py
@@ -1,7 +1,18 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 
 
-def chainplot(chains, labels: list[str] | None = None):
+def chainplot(
+    chains: np.ndarray, labels: list[str] | None = None
+) -> tuple[Figure, Axes]:
+    """Plot MCMC chains
+
+    :param chains: Array of MCMC chains, with the "parameters" dimension as the last one
+    :param labels: Name of the parameters
+    :return: Figure and Axes for the plot
+    """
     ndim = chains.shape[-1]
     if labels is None:
         labels = [f"x{i}" for i in range(ndim)]
