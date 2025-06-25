e9c481381f82a8dd271db5d01dd9a059f4c97a3b
Thomas Vandal <thomas.vandal@umontreal.ca>
2025-06-24 20:07:09 -0400

    Simplify and clarify Model usage in tutorials

--- src/simple/__init__.py
+++ src/simple/__init__.py
@@ -1,1 +1,3 @@
+__all__ = ["__version__", "ForwardModel", "GaussianForwardModel", "Model"]
 from simple._version import __version__ as __version__
+from simple.model import ForwardModel, GaussianForwardModel, Model
